a763cbc0dcb1d608ceb5ac7dc6aa57a83c10beb0
Daryl Eisner <ucsdmath.project@gmail.com>
2016-08-20 11:12:51 -0700

    Update

--- src/Pdf/AbstractPdfAdapter.php
+++ src/Pdf/AbstractPdfAdapter.php
@@ -65,7 +65,7 @@ abstract class AbstractPdfAdapter implements PdfInterface, ServiceFunctionsInter
      *
      * @api
      */
-    const VERSION = '1.8.0';
+    const VERSION = '1.9.0';
 
     //--------------------------------------------------------------------------
 
