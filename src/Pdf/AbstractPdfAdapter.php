4966bfdd08fd13979d4aa6e38f1d1aaf8fa8f7ef
UCSDMath <UCSDMath.Project@gmail.com>
2017-03-31 17:04:59 -0700

    Update

--- src/Pdf/AbstractPdfAdapter.php
+++ src/Pdf/AbstractPdfAdapter.php
@@ -65,7 +65,7 @@ abstract class AbstractPdfAdapter implements PdfInterface, ServiceFunctionsInter
      *
      * @api
      */
-    public const VERSION = '1.17.0';
+    public const VERSION = '1.18.0';
 
     //--------------------------------------------------------------------------
 
